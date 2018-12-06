0efb44fe543e86e8772ced27dc3b055f1e9cd935
Safak Can Bas <can.bas@adesso.com.tr>
2018-12-06 15:22:06 +0300

    * Change some features for release

--- AppWeather/ViewController.swift
+++ AppWeather/ViewController.swift
@@ -110,6 +110,7 @@ extension ViewController: UITableViewDataSource,UITableViewDelegate,CLLocationMa
         
         var tempWeather = Weather(latitude: 0.0, longitude: 0.0)
     
+        
         if editingStyle == UITableViewCellEditingStyle.delete {
             //******hayır bu da calısmıyo ilkini siliyo normal table dan da silince
             if isSearching == false {
